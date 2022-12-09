182410e9f567afaf26ee4f66aad3045c5bada8bf
Karan Singh Kochar <pyrotank41@gmail.com>
2022-12-09 01:36:13 -0600

    update utils

--- utils.py
+++ utils.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""milestones 1, 2, &3.ipynb
-
-Original file is located at
-    https://colab.research.google.com/drive/1otAPw-SGgQ568io10qxsWIEq0nSjdXGd
-
-"""
-
+# helper functions to make the notebook more readable
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
@@ -15,6 +8,17 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy.stats
+import matplotlib.offsetbox as offsetbox
+from matplotlib.ticker import StrMethodFormatter
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, TensorDataset
+from torch.utils.data import DataLoader
 
 def get_data(location_pair): 
     # Tries to get local version and then defaults to google drive version
@@ -119,6 +123,8 @@ def final_preprocess(predictors, drug_overdose_data):
     # scale the label column "Overdose Deaths" by dividing by 10000:
     final_data["Overdose Deaths"] = final_data["Overdose Deaths"]/10000
     final_data = final_data.rename({'Overdose Deaths': 'Overdose Deaths e10-4'}, axis=1)
+    try: final_data.drop(['Unnamed: 0'], axis=1, inplace=True) 
+    except: pass
     return final_data
 
 def visualize_correlation(data):
@@ -128,16 +134,20 @@ def visualize_correlation(data):
     """
     sns.set_palette('colorblind')
     sns.pairplot(data=data, height=3)
-    plt.show()
 
-def get_dataframe(data_pickle_file_name):
+def get_dataframe(data_file_name, use_pickle=True, safe_pickle=True):
+    
+    if use_pickle:
+        data_pickle_file_name = data_file_name
+    else: 
+        data_pickle_file_name = "" # this condition forces the function into exception handling.
     
     try:
         data = pd.read_pickle(data_pickle_file_name)
         print("Dataframe loaded from pickle file.")
 
     except FileNotFoundError:
-        print("Dataframe pickle file not found, creating dataframe and pickleing the file for future use...")
+        print("Dataframe pickle file not found, creating dataframe and pickling the file for future use...")
 
         local_file_name = 'VSRR_Provisional_Drug_Overdose_Death_Counts.csv'
         remote_file_name = 'https://drive.google.com/file/d/1ah5KesrTEmAKraaZM4HSPRhWMihc36gS/view?usp=sharing'
@@ -148,15 +158,84 @@ def get_dataframe(data_pickle_file_name):
         remote_file_name = 'https://drive.google.com/file/d/1WEDm7_xasrjbV6WsWw773Q1eWa5qaWMU/view?usp=sharing'
 
         data = final_preprocess(get_data((local_file_name, remote_file_name)), drug_overdose_data)
-        data.to_pickle(data_pickle_file_name)
+        if safe_pickle: 
+            data.to_pickle(data_file_name)
+            print(f"saved pickel file as {data_file_name}.")
 
     return data
+
+class NeuralNetwork(nn.Module):
+        def __init__(self, X_input_dim):
+            super(NeuralNetwork, self).__init__()
+            self.layer1 = nn.Linear(X_input_dim, 32) # input: X.shape[1]=13, output: 32
+            self.layer2 = nn.Linear(32, 32)
+            self.layer3 = nn.Linear(32, 16)
+            self.layer4 = nn.Linear(16, 1)
+
+        def forward(self, x):
+            x = F.relu( self.layer1(x) )
+            x = F.relu( self.layer2(x) )
+            x = F.relu( self.layer3(x) )
+            x = self.layer4(x)
+
+            return x
+
+def train_step(x, y, nnmodel, loss_function, optimizer):
+  # initialize the gradient of model parameters
+  optimizer.zero_grad()
+  # make prediction
+  y_val = nnmodel(x)
+  # calculate the loss
+  loss = loss_function(y_val, y)
+  # Backpropagation
+  loss.backward()
+  # Update parameters
+  optimizer.step()
+  
+  return loss.item()
+
+def train(x, y, nnmodel, loss_function, optimizer, epochs=20, print_iter_freq=20, batch=7):
+  # Setup Mini-Batch Learning:
+  
+  # Wrap x_train and y_train in a TensorDataset object
+  train_data = TensorDataset(x, y)
+  # Create DataLoader with batch_size
+  train_loader = DataLoader(dataset=train_data, batch_size=batch, shuffle=True)
+  # print(next(iter(train_loader)))
+
+  losses = []
+  # Epochs iterations
+  for i in range(epochs):
+    for x_batch, y_batch in train_loader:  
+      loss = train_step(x_batch, y_batch, nnmodel, loss_function, optimizer)
+      losses.append(loss)
+    
+    # loss = train_step(x_train, y_train, model=model, loss_function=mse_loss, optimizer=sgd_optimizer)
+    # # losses.append(loss.detach().numpy())
+    # losses.append(loss)
+
+    if (i % print_iter_freq) == 0:
+      print('epoch: {},'.format(i) + 'loss: {:.5f}'.format(loss))
+
+  return losses
+
+def visualizer(loss):
+  plt.figure()
+  plt.plot(loss,color="red")
+  plt.xlabel("Iteration")
+  plt.ylabel("loss")
+  plt.ylim = ((0,50))
+  plt.xlim = ((0,2000))
+  title=[]
+  title.append("Losses")
+  plt.title("\n".join(title))
+  plt.axhline(0,linewidth=2,linestyle=":",color="black")
+  plt.show()
+  plt.close()
 
 def main():
 
     data = get_dataframe('data.pickle')
-
-
 
 if __name__ == "__main__":
     main()
